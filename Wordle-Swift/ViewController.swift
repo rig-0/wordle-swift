10432ad8226f0cd830ce9cc1a95b8d93dc4af9bd
rig-0 <rigo@rig0.com>
2023-08-11 17:52:18 -0700

    Initial commit - empty project with gitignore

---
+++ Wordle-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wordle-Swift
+//
+//  Created by RIGO CARBAJAL on 8/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wordle-SwiftUITests/Wordle_SwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Wordle_SwiftUITestsLaunchTests.swift
+//  Wordle-SwiftUITests
+//
+//  Created by RIGO CARBAJAL on 8/11/23.
+//
+
+import XCTest
+
+final class Wordle_SwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
