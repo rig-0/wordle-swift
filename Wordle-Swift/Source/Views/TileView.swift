eccc7a9d2179af35c05570f29451af44b712b258
rig-0 <rigo@rig0.com>
2023-08-15 19:39:23 -0700

    Slight tweak on reveal speed

--- Wordle-Swift/Source/Views/TileView.swift
+++ Wordle-Swift/Source/Views/TileView.swift
@@ -93,7 +93,7 @@ class TileView: UIView {
     }
     
     public func animateReveal(state: KeyState, completion: @escaping (() -> Void)) {
-        UIView.animate(withDuration: 0.15, animations: {
+        UIView.animate(withDuration: 0.2, animations: {
             self.wrapperView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1, 0.001);
         }, completion: { finished in
             self.state = state
