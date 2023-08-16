3b8fdcc64a5de28f97619e351b35835c1bfe3cc0
rig-0 <rigo@rig0.com>
2023-08-15 17:06:20 -0700

    Added header bar

--- Wordle-Swift/Source/Extensions/UIFont+AppFonts.swift
+++ Wordle-Swift/Source/Extensions/UIFont+AppFonts.swift
@@ -7,6 +7,7 @@ import UIKit
 
 extension UIFont {
     struct App {
+        static var wordleFont: UIFont { UIFont(name: "KarnakPro-CondensedBlack", size: 40)! }
         static var keyEnterFont: UIFont { UIFont(name: "HelveticaNeue-Bold", size: 10)! }
         static var keyStandardFont: UIFont { UIFont(name: "HelveticaNeue-Bold", size: 20)! }
         static var tileFont: UIFont { UIFont(name: "HelveticaNeue-Bold", size: 40)! }
--- Wordle-Swift/ViewController.swift
+++ Wordle-Swift/ViewController.swift
@@ -10,6 +10,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let headerView = UIView()
+        self.view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        headerView.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        let labelView = UILabel()
+        labelView.font = UIFont.App.wordleFont
+        labelView.text = "Wordle // Swift"
+        labelView.textColor = .black
+        headerView.addSubview(labelView)
+        labelView.translatesAutoresizingMaskIntoConstraints = false
+        labelView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+        labelView.bottomAnchor.constraint(equalTo: headerView.bottomAnchor, constant: -12).isActive = true
+        
+        let dividerView = UIView()
+        headerView.addSubview(dividerView)
+        dividerView.backgroundColor = UIColor(rgb: (212, 214, 218))
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        dividerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        dividerView.bottomAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
+        dividerView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        
         let gameView = GameView()
         self.view.addSubview(gameView)
         gameView.translatesAutoresizingMaskIntoConstraints = false
