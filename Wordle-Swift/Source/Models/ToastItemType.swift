072dfde51c80eac53a34aefe0a5928f6c4cfc27d
rig-0 <rigo@rig0.com>
2023-08-17 17:32:02 -0700

    Cleanup on Toast files

---
+++ Wordle-Swift/Source/Models/ToastItemType.swift
@@ -0,0 +1,17 @@
+//
+//  ToastItemType.swift
+//  ©️ 2023 0100
+//
+
+import Foundation
+
+enum ToastItemType: String {
+    case notInWordList = "Not in word list"
+    case notEnoughLetters = "Not enough letters"
+    case winGenius = "Genius"
+    case winMagnificent = "Magnificent"
+    case winImpressive = "Impressive"
+    case winSplendid = "Splendid"
+    case winGreat = "Great"
+    case winPhew = "Phew"
+}
--- Wordle-Swift/Source/Views/ToastStackView.swift
+++ Wordle-Swift/Source/Views/ToastStackView.swift
@@ -39,62 +39,6 @@ class ToastStackView: UIView {
         self.stackView.addArrangedSubview(toastItem)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
             toastItem.pop()
-            print(self.stackView.arrangedSubviews.count)
-        })
-    }
-}
-
-enum ToastItemType: String {
-    case notInWordList = "Not in word list"
-    case notEnoughLetters = "Not enough letters"
-    
-    case winGenius = "Genius"
-    case winMagnificent = "Magnificent"
-    case winImpressive = "Impressive"
-    case winSplendid = "Splendid"
-    case winGreat = "Great"
-    case winPhew = "Phew"
-}
-
-class ToastItemView: UIView {
-    
-    var type: ToastItemType
-    
-    init(type: ToastItemType) {
-        self.type = type
-        super.init(frame: .zero)
-        self.setupView()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupView() {
-        self.backgroundColor = .black
-        self.layer.cornerRadius = 3
-        
-        let errorLabel = UILabel()
-        self.addSubview(errorLabel)
-        errorLabel.font = UIFont.App.toastFont
-        errorLabel.text = type.rawValue
-        errorLabel.textColor = .white
-        errorLabel.translatesAutoresizingMaskIntoConstraints = false
-        errorLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
-        errorLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
-        errorLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
-        errorLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
-    }
-    
-    public func pop() {
-        UIView.animate(withDuration: 0.2, animations: {
-            self.alpha = 0
-        }, completion: { finished in
-            UIView.animate(withDuration: 0.1, animations: {
-                self.isHidden = true
-            }, completion: { finished in
-                self.removeFromSuperview()
-            })
         })
     }
 }
