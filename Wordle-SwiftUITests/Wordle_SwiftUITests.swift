78b80c6b684983bf329581cd055242e4fc11abf0
rig-0 <rigo@rig0.com>
2023-08-23 12:37:45 -0700

    Add test case for phew solve

--- Wordle-SwiftUITests/Wordle_SwiftUITests.swift
+++ Wordle-SwiftUITests/Wordle_SwiftUITests.swift
@@ -51,6 +51,80 @@ final class Wordle_SwiftUITests: XCTestCase {
         XCTAssertEqual(result, .completed)
     }
     
+    func testPhewSolve() throws {
+        
+        // Wait for keyboard to appear
+        guard element(key: .A).waitForExistence(timeout: 1) else {
+            XCTFail()
+            return
+        }
+        
+        // Tap keys for solve word and press ENTER
+        element(key: .A).tap()
+        element(key: .M).tap()
+        element(key: .P).tap()
+        element(key: .L).tap()
+        element(key: .E).tap()
+        element(key: .ENTER).tap()
+        
+        Thread.sleep(forTimeInterval: 1)
+        
+        element(key: .H).tap()
+        element(key: .I).tap()
+        element(key: .P).tap()
+        element(key: .P).tap()
+        element(key: .O).tap()
+        element(key: .ENTER).tap()
+        
+        Thread.sleep(forTimeInterval: 1)
+        
+        element(key: .A).tap()
+        element(key: .P).tap()
+        element(key: .P).tap()
+        element(key: .L).tap()
+        element(key: .Y).tap()
+        element(key: .ENTER).tap()
+        
+        Thread.sleep(forTimeInterval: 1)
+        
+        element(key: .T).tap()
+        element(key: .U).tap()
+        element(key: .P).tap()
+        element(key: .L).tap()
+        element(key: .E).tap()
+        element(key: .ENTER).tap()
+        
+        Thread.sleep(forTimeInterval: 1)
+        
+        element(key: .P).tap()
+        element(key: .O).tap()
+        element(key: .P).tap()
+        element(key: .P).tap()
+        element(key: .Y).tap()
+        element(key: .ENTER).tap()
+        
+        Thread.sleep(forTimeInterval: 1)
+        
+        element(key: .A).tap()
+        element(key: .P).tap()
+        element(key: .P).tap()
+        element(key: .L).tap()
+        element(key: .E).tap()
+        element(key: .ENTER).tap()
+        
+        Thread.sleep(forTimeInterval: 1)
+        
+        // Wait for Magnificent toast to appear
+        let expectation = expectation(
+            for: NSPredicate(format: "exists == true"),
+            evaluatedWith: app.staticTexts["Phew"],
+            handler: .none
+        )
+
+        let result = XCTWaiter.wait(for: [expectation], timeout: 3.0)
+        XCTAssertEqual(result, .completed)
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
